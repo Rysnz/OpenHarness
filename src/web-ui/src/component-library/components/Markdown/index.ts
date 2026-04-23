d3b0421db4d3e4042fe9c82643889bac7d1326d7
User <user@example.com>
2026-04-23 13:25:23 +0800

    Move markdown rendering components into workbench

--- src/web-ui/src/component-library/components/Markdown/index.ts
+++ src/web-ui/src/component-library/components/Markdown/index.ts
@@ -1,14 +1,1 @@
-/**
- * Markdown component exports
- */
-export { Markdown } from './Markdown';
-export type { MarkdownProps, LineRange } from './Markdown';
-
-export { Markdown as MarkdownRenderer } from './Markdown';
-export type { MarkdownProps as MarkdownRendererProps } from './Markdown';
-
-export { MermaidBlock } from './MermaidBlock';
-export type { MermaidBlockProps } from './MermaidBlock';
-
-export { ReproductionStepsBlock } from './ReproductionStepsBlock';
-export type { ReproductionStepsBlockProps } from './ReproductionStepsBlock';
+export * from '@/workbench/rendering/markdown/index';
